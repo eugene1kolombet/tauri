9de90bec01da65760811141769ecf25038e89ec9
KEV <eugene1kolombet@gmail.com>
2023-11-10 15:10:26 +0300

    1

--- core/tauri/src/updater/core.rs
+++ core/tauri/src/updater/core.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-extern crate msgbox;
-
 use super::error::{Error, Result};
 #[cfg(desktop)]
 use crate::api::file::{ArchiveFormat, Extract, Move};
