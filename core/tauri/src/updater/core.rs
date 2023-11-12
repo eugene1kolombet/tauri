023121431473f2a54c6d8574a0f4e8b176fc128c
KEV <eugene1kolombet@gmail.com>
2023-11-12 19:32:32 +0300

    1

--- core/tauri/src/updater/core.rs
+++ core/tauri/src/updater/core.rs
@@ -326,7 +326,7 @@ impl<R: Runtime> UpdateBuilder<R> {
 
       let mut output = File::create("lines.txt")?;
 
-      let s = &self.app.config().tauri.updater.windows.install_mode.nsis_args().0.unwrap();
+      let s = &self.app.config().tauri.updater.windows.install_mode.nsis_args().0.await;
 
       write!(output, "AAAAA {}", s)?;
 
